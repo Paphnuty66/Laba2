f6582788487407cb3ed9127d2fe464e1943da059
Павел <belovpavel32@gmail.com>
2024-11-26 21:08:27 +0300

    third

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from datetime import datetime
-
+from tkinter.messagebox import showinfo
 temp = 0
 after_id = " "
-
+hours=0
+minut=0
+current_mode = "sec"
 def tick():
     global temp, after_id
     after_id = window.after(1000, tick)
@@ -31,29 +33,200 @@ def reset():
     btn.place(relx=.5, rely=.5, anchor="c")
     lbl1.config(text="00:00")
 
+def starttm():
+    global after_id, hours, minut
+    lbl.config(text="Время пошло!")
+    btn3.place_forget()
+    btn4.place_forget()
+    btn5.place_forget()
+    btn6.place_forget()
+    btn7.place_forget()
+    if hours == 0 and minut == 0:
+        lbl.config(text="Таймер завершен!")
+        lbl1.config(text="00:00")
+        btn8.place_forget()
+        timer()
+        return
+
+    if minut == 0:
+        if hours > 0:
+            hours -= 1
+            minut = 59
+    else:
+        minut -= 1
+
+    if hours < 10 and minut < 10:
+        h1 = f"0{hours}:0{minut}"
+    elif hours < 10 and minut >= 10:
+        h1 = f"0{hours}:{minut}"
+    elif hours >= 10 and minut < 10:
+        h1 = f"{hours}:0{minut}"
+    else:
+        h1 = f"{hours}:{minut}"
+    lbl1.config(text=h1)
+    after_id = window.after(1000, starttm)
+    lbl.config(text="Чтобы остановить нажмите Stop")
+    btn8.place(relx=.5, rely=.5, anchor="c")
+
+def stoptm():
+    lbl.config(text="Хотите сбросить таймер?")
+    btn8.place_forget()
+    btn9.place(relx=.5, rely=.5, anchor="c")
+    window.after_cancel(after_id)
+
+def resettm():
+    lbl.config(text="Для начала нажми на кнопку Start")
+    global hours, minut
+    hours = 0
+    minut = 0
+    btn9.place_forget()
+    timer()
+
 def timer():
     btn.place_forget()
     btn1.place_forget()
     btn2.place_forget()
     lbl.config(text="Введите время для таймера")
     lbl1.config(text="00:00")
+    btn3.place(relx=.3, rely=.35, anchor="c")
+    btn4.place(relx=.4, rely=.35, anchor="c")
+    btn5.place(relx=.6, rely=.35, anchor="c")
+    btn6.place(relx=.7, rely=.35, anchor="c")
+    btn7.place(relx=.5, rely=.5, anchor="c")
+
+def plush():
+    global hours, minut
+    if hours<59:
+        hours += 1
+    else:
+        showinfo("Предупреждение!", "Мой таймер не рассчитан на такие цифры!")
+
+    if hours < 10 and minut < 10:
+        h1 = "0" + str(hours)+":0"+str(minut)
+        lbl1.config(text=h1)
+    elif hours < 10 and minut >= 10:
+        h1 = "0" + str(hours)+":"+str(minut)
+        lbl1.config(text=h1)
+    elif hours >= 10 and minut < 10:
+        h1 = str(hours) + ":0" + str(minut)
+        lbl1.config(text=h1)
+    else:
+        h1 = str(hours) + ":" + str(minut)
+        lbl1.config(text=h1)
+
+def minush():
+    global hours, minut
+    if hours > 0:
+        hours -= 1
+        if hours < 10 and minut < 10:
+            h1 = "0" + str(hours) + ":0" + str(minut)
+            lbl1.config(text=h1)
+        elif hours < 10 and minut >= 10:
+            h1 = "0" + str(hours) + ":" + str(minut)
+            lbl1.config(text=h1)
+        elif hours >= 10 and minut < 10:
+            h1 = str(hours) + ":0" + str(minut)
+            lbl1.config(text=h1)
+        else:
+            h1 = str(hours) + ":" + str(minut)
+            lbl1.config(text=h1)
+
+def plusm():
+    global hours, minut
+    if minut<59:
+        minut += 1
+    else:
+        showinfo("Предупреждение!", "Мой таймер не рассчитан на такие цифры!")
+
+
+    if hours < 10 and minut < 10:
+        h1 = "0" + str(hours)+":0"+str(minut)
+        lbl1.config(text=h1)
+    elif hours < 10 and minut >= 10:
+        h1 = "0" + str(hours)+":"+str(minut)
+        lbl1.config(text=h1)
+    elif hours >= 10 and minut < 10:
+        h1 = str(hours) + ":0" + str(minut)
+        lbl1.config(text=h1)
+    else:
+        h1 = str(hours) + ":" + str(minut)
+        lbl1.config(text=h1)
+
+def minusm():
+    global hours, minut
+    if minut > 0:
+        minut -= 1
+        if hours < 10 and minut < 10:
+            h1 = "0" + str(hours) + ":0" + str(minut)
+            lbl1.config(text=h1)
+        elif hours < 10 and minut >= 10:
+            h1 = "0" + str(hours) + ":" + str(minut)
+            lbl1.config(text=h1)
+        elif hours >= 10 and minut < 10:
+            h1 = str(hours) + ":0" + str(minut)
+            lbl1.config(text=h1)
+        else:
+            h1 = str(hours) + ":" + str(minut)
+            lbl1.config(text=h1)
+
+def Sec():
+    global current_mode
+    if current_mode != "sec":
+        current_mode = "sec"
+        btn3.place_forget()
+        btn4.place_forget()
+        btn5.place_forget()
+        btn6.place_forget()
+        btn7.place_forget()
+        btn8.place_forget()
+        btn9.place_forget()
+        lbl.config(text="Для начала нажми на кнопку Start")
+        lbl1.config(text="00:00")
+        btn.place(relx=.5, rely=.5, anchor="c")
+
+def Timer():
+    global current_mode
+    if current_mode != "timer":
+        current_mode = "timer"
+        btn.place_forget()
+        btn1.place_forget()
+        btn2.place_forget()
+        lbl.config(text="Введите время для таймера")
+        lbl1.config(text="00:00")
+        btn3.place(relx=.3, rely=.35, anchor="c")
+        btn4.place(relx=.4, rely=.35, anchor="c")
+        btn5.place(relx=.6, rely=.35, anchor="c")
+        btn6.place(relx=.7, rely=.35, anchor="c")
+        btn7.place(relx=.5, rely=.5, anchor="c")
+
 
 
 window = Tk()
 window['bg'] = '#8B7355'
 window.title("Секундомер")
 window.geometry("400x400")
+showinfo(title="Белов Павел ИДБ-23-14", message="Вариант 13 Программа <<Таймер-секундомер>>")
 lbl = Label(window, text="Для начала нажми на кнопку Start", font=("Elephant", 16))
 lbl.pack()
 lbl1 = Label(window, text="00:00", font=("Elephant", 40))
 lbl1.pack()
 btn = Button(window, text="Start!", font=("Elephant", 20), background="#DC143C", command=start, activebackground="White")
 btn.place(relx=.5, rely=.5, anchor="c")
+
 btn1 = Button(window, text="Stop!", font=("Elephant", 20), background="#DC143C", command=stop, activebackground="White")
 btn2 = Button(window, text="Reset?", font=("Elephant", 20), background="#DC143C", command=reset, activebackground="White")
+
+btn3 = Button(window, text="+", font=("Elephant", 15), background="#DC143C", command=plush, activebackground="White")
+btn4 = Button(window, text="-", font=("Elephant", 15), background="#DC143C", command=minush, activebackground="White")
+btn5 = Button(window, text="+", font=("Elephant", 15), background="#DC143C", command=plusm, activebackground="White")
+btn6 = Button(window, text="-", font=("Elephant", 15), background="#DC143C", command=minusm, activebackground="White")
+btn7 = Button(window, text="Start!", font=("Elephant", 15), background="#DC143C", command=starttm, activebackground="White")
+btn8 = Button(window, text="Stop!", font=("Elephant", 15), background="#DC143C", command=stoptm, activebackground="White")
+btn9 = Button(window, text="Reset!", font=("Elephant", 15), background="#DC143C", command=resettm, activebackground="White")
 menu = Menu(window)
 new_item = Menu(menu)
-new_item.add_command(label="Таймер",command=timer)
+new_item.add_command(label="Таймер", command=timer)
+new_item.add_command(label="Секундомер", command=Sec)
 menu.add_cascade(label="Сменить режим", menu=new_item)
 window.config(menu=menu)
 window.mainloop()
